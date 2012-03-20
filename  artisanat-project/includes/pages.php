c2d07a8e6b22e5bd80e7712bdc6856372a83ff9e
renaud.hayeme <renaud.hayeme@gmail.com>
2012-03-20 13:32:52 +0000

    coucou

---  artisanat-project/includes/pages.php
+++  artisanat-project/includes/pages.php
@@ -8,5 +8,11 @@
 	elseif($_GET['page'] == 'contact') {
 	include("modules/contact/contact.php");
 	}
+	elseif($_GET['page'] == 'deconnexion') {
+	include("modules/membre/public/deconnexion.php");
+	}
+	elseif($_GET['page'] == 'inscription') {
+	include("modules/membre/public/inscription.php");
+	}
 	
 	/* Fin lien module contact */
